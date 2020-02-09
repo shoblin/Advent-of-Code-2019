402717b4f031bc57a1fcb4066ab8c2265652eb98
Топольский Антон Сергеевич (atopolskiy) <atopolskiy@corp.whotrades.eu>
2020-02-09 18:20:38 +0300

    Day 3. Full solution

--- day3.py
+++ day3.py
@@ -9,61 +9,60 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import lib.functions_au as fa
+import operator
 
-def new_coordinate(code, last_x, last_y):
-    '''
-    Function for transform instruction into coordinates
-	R - add value to coordinate of X
-	L - substract valur from coordinate of X
-	U - add value to coordinate ofY
-	D - substract valur from coordinate ofY
-    '''
-
-    if code[0] == 'R':
-        new_x = last_x + int(code[1:])
-        new_y = last_y
-    elif code[0] == 'L':
-        new_x = last_x - int(code[1:])
-        new_y = last_y
-    elif code[0] == 'U':
-        new_x = last_x
-        new_y = last_y + int(code[1:])
-    elif code[0] == 'D':
-        new_x = last_x
-        new_y = last_y - int(code[1:])
-
-    return new_x, new_y
-
-def build_wire(list):
+def build_wire(list, wire):
     '''
     Function to create two lists with coordinates of points
     '''
-    wire_x_cord = [0]
-    wire_y_cord = [0]
+    x, y = 0, 0
+    steps = 0
 
     for code in list:
-        new_coordinate(code, wire_x_cord[-1], wire_y_cord[-1])
-        nx, ny = new_coordinate(code, wire_x_cord[-1], wire_y_cord[-1])
-        wire_x_cord.append(nx)
-        wire_y_cord.append(ny)
+        lenght = int(code[1:])
 
-    return (wire_x_cord, wire_y_cord)
+        for _ in range(lenght):
+            if code[0] == 'R':
+                x += 1
+            elif code[0] == 'L':
+                x -= 1
+            elif code[0] == 'U':
+                y += 1
+            elif code[0] == 'D':
+                y -= 1
+            steps += 1
+            wire[(x,y)] = steps
 
+    return wire
+
+def manhattan_distance(coord):
+    return abs(coord[0]) + abs(coord[1])
 
 
 def main():
-    list1 = fa.read_input_values('data\\day3_puzzle_input.txt')
-    list2 = fa.read_input_values('data\\day3_puzzle_input.txt', 1)
+    template = "Part{} Answer: {}"
 
-    wire1 = []
-    wire2 = []
-    wire1.append(build_wire(list1))
-    print(wire1)
-    wire2.append(build_wire(list2))
+    list1 = fa.read_input_values('day3_puzzle_input.txt')
+    list2 = fa.read_input_values('day3_puzzle_input.txt', 1)
 
-    print(wire2)
+    wire1 = build_wire(list1, {})
+    wire2 = build_wire(list2, {})
+
+    cross_wire = list(set(wire1.keys()) & set(wire2.keys()))
+
+    result1 = manhattan_distance(cross_wire[0])
+    for coord in cross_wire:
+        result1 = min(result1, manhattan_distance(coord))
+
+    print(template.format(1, result1))
 
 
+    steps = wire1[cross_wire[0]] + wire2[cross_wire[0]]
+    for coord in cross_wire:
+        if steps > wire1[coord] + wire2[coord]:
+            steps = wire1[coord] + wire2[coord]
+
+    print(template.format(2, steps))
 
 
 if __name__ == '__main__':
