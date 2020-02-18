d5db006194e22810c10aecca67e4d3a5e7c91512
Топольский Антон Сергеевич (atopolskiy) <atopolskiy@corp.whotrades.eu>
2020-02-18 19:06:23 +0300

    Add day16 Part1

--- day16.py
+++ day16.py
@@ -8,28 +8,44 @@
 # Copyright:   (c) atopolskiy 2020
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import lib.functions_au as fa
+
 def transform_task(task):
     return [int(x) for x in task]
 
 def creat_pattern(pattern, repeats):
     '''
-
+    Calculate parttern for new_step
     '''
     result = []
     for pat in pattern:
         result += [pat] * repeats
     return result
+def calculate_number(input_list, new_pattern):
+    '''
+    '''
+    point = 1
+    result = 0
+    for curr_num in input_list:
+        if point >= len(new_pattern):
+            point = 0
+        result += curr_num * new_pattern[point]
+        point += 1
+    return abs(result) % 10
 
-def calculate_output(input_list):
+
+def calculate_phase(input_list):
     '''
     Calculate output for all symbols
     '''
     pattern = [0, 1, 0, -1]
     result = []
-    point = 1
 
-    for num in input_list:
-        new =
+    for num in range(1,len(input_list)+1):
+        new_pattern = creat_pattern(pattern, num)
+        result.append(calculate_number(input_list, new_pattern))
+
+    return result
 
 
 def fft(task_str, num_phases):
@@ -37,16 +53,33 @@ def fft(task_str, num_phases):
 
     '''
     task_list = transform_task(task_str)
-    num_str = len(task_list)
 
     for phase in range(1, num_phases + 1):
-##        calulate creat_pattern(pattern, phase))
+        task_list = calculate_phase(task_list)
+
+    return task_list[:8]
 
 def main():
     task_test1 = '12345678'
-    task_test2 = '80871224585914546619083218645595'
+    task_test2 = list('03036732577212944063491565474664')
 
-    t = fft(task_test1, 4)
+##    task = fa.read_input_values('day16_puzzle_input.txt')
+##
+##    t = fft(task[0], 100)
+##    result = ''.join(map(str, t))
+##    print('Part1:', result)
+
+    task_test2 = task_test2 * 10000
+    offset = int(''.join([str(d) for d in task_test2[:7]]))
+    task_list = task_test2[offset:]
+##    print(len(task_test2), offset)
+
+
+##        print(new_signal)
+
+
+##    print('Part 2: ', ''.join(map(str, task_test2[:8])))
+
 
 
 if __name__ == '__main__':
