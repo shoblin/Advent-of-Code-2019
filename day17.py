d89b8dcd567e75112bc083e881c26fdb6cbe99e5
Топольский Антон Сергеевич (atopolskiy) <atopolskiy@corp.whotrades.eu>
2020-03-02 06:55:18 +0300

    Day 17. Part 2 - First try

--- day17.py
+++ day17.py
@@ -11,43 +11,15 @@
 from lib.intcode import Computer
 import lib.functions_au as fa
 
-def create_map(optcode):
-    '''
-    '''
-    map_codes = []
-    line_code = []
+### Part1: To calibrate the cameras by getting the alignment parameters of some
+### well-defined points.
 
-    computer = Computer(optcode)
-    while not computer.done:
-        computer.calculate()
-        if not computer.done:
-
-            if computer.output == 10:
-                if len(line_code) != 0:
-                    map_codes.append(line_code[:])
-                line_code =[]
-
-            else:
-                line_code.append(computer.output)
-
-    return map_codes
-
-def draw_map(map_code):
-    '''
-    35 - #,
-    46 - .,
-    10 - starts a new line
-    '''
-
-    for line in map_code:
-        str_line = ''
-        for ch in line:
-            str_line += chr(ch)
-
-        print(str_line)
 
 def cross_check(map_code, x, y):
     '''
+    Part 1: Find coordinates of crossing lines.
+    Return True if find line crossing
+    Return False if don't find line crossing
     '''
     #cross coordinates
     cross = [(0,1), (1,0), (0,-1), (-1,0)]
@@ -62,9 +34,9 @@ def cross_check(map_code, x, y):
     return True
 
 
-
-def calculate_coordinates(map_code):
+def calculate_result_part1(map_code):
     '''
+    Part1: Calculate results SUM of multiplication of X and Y
     '''
     # Find max X and Y for cycles
     max_y = len(map_code) - 1
@@ -81,7 +53,75 @@ def calculate_coordinates(map_code):
     for coord in coords:
         result += coord[0] * coord[1]
 
-    print(result)
+    return result
+
+
+def create_map(optcode):
+    '''
+    Create map: calculate map. Divide into lines, 10 - starts a new line
+    '''
+    map_codes = []
+    line_code = []
+
+    computer = Computer(optcode)
+    while not computer.done:
+        computer.calculate()
+        if not computer.done:
+
+            if computer.output == 10:
+                if len(line_code) != 0:
+                    map_codes.append(line_code[:])
+                line_code =[]
+            else:
+                line_code.append(computer.output)
+
+    return map_codes
+
+
+def draw_map(map_code):
+    '''
+    Draw map and find coordinates of robot
+    35 - #,
+    46 - .,
+    10 - starts a new line
+    '''
+    bot_x, bot_y = 0, 0
+    y = 0
+
+    for line in map_code:
+        str_line = ''
+        x = 0
+        for ch in line:
+            str_line += chr(ch)
+            # Find symbol '^' and remember coordinates
+            if ch == 94:
+                bot_x, bot_y = x, y
+            x += 1
+        y += 1
+        print(str_line)
+    return bot_x, bot_y
+
+
+def calculate_path(map_code, bot_x, bot_y):
+    '''
+    Calculate Bot's path.Movement functions may use L to turn left, R to turn right,
+    or a number to move forward that many units.
+    '''
+    current_direction = (0, -1)
+    current_position = (bot_x, bot_y)
+    directions = [(-1,0), (0,1), (1,0), (0,-1)]
+    bot_path = []
+    end_path = False
+
+    while not end_path:
+        x, y = current_position[0] + current_direction[0], current_position[1] + current_direction[1]
+
+        if map_code[y][x] != 35:
+##            if
+
+        end_path = True
+
+
 
 
 def main():
@@ -90,14 +130,14 @@ def main():
     opcode_list = [int(thing) for thing in opcode_list]
 
     task_map_code = create_map(opcode_list)
-    draw_map(task_map_code)
+    bot_x, bot_y = draw_map(task_map_code)
 
-    calculate_coordinates(task_map_code)
+    result1 = calculate_result_part1(task_map_code)
 
+    ### Print First Result ###
+    print('Part1: ', result1)
 
-
-
-
+    calculate_path(opcode_list, bot_x, bot_y)
 
 
 if __name__ == '__main__':
